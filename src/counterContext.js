bf2950195098f55a61989e830910eacf1a2de5c4
harish242 <markollh242@gmail.com>
2023-06-12 14:38:14 +0530

    jkdfd

--- src/counterContext.js
+++ src/counterContext.js
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext,useState } from "react";
 
 
 
@@ -8,7 +8,10 @@ const counterContext = createContext()
 
 
 const CounterStateContext = (props) => {
-
+const[count,setCount]=useState(0)
+// const HandleClick=()=>{
+//     setCount(prev=>prev+1)
+// }
     return (
         <div id="counter-context">
             <counterContext.Provider value={{ count, setCount }}>
